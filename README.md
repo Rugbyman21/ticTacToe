8e93b15f4fb1340e6a2352ba76315ec96eb7ccfa
JuliannaGreen & Austin Kincaid <student@epicodus.com>
2015-09-16 16:46:47 -0700

    Add Readme and comments for friends who work on this next

---
+++ README.md
@@ -0,0 +1,44 @@
+#_Tic-Tac-Toe_
+
+#### _An incomplete implementation of tic-tac-toe, 9/16/2015_
+
+#### By _Julianna Green & Austin Kincaid_
+
+## Description
+
+_Back-end is built most of the way and the front-end is in development. The aim is to have an app that can track two player's progress through a game of tic-tac-toe._
+
+_Issues to fix (that we're aware of):_
+* Track player turns (this was brought up to us late and should have been included earlier, sorry!)
+* Front-end is being built with HTML elements and has only just been basically prepared. Please continue this!
+
+## Setup
+
+_This application uses JavaScript, Jquery, Mocha JS, Chai JS, and finally bootstrap for styling._
+
+* Enjoy!
+
+### Legal
+
+
+Copyright (c) 2015 **Austin Kincaid & Julianna Green**
+
+This software is licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,21 @@
+// Players only have a selection. This can be anything, including the classic Xs and Os
 function Player(selection) {
   this.selection = selection;
 };
 
+// Spaces have coordinates and carry a player's selection (if given one).
 function Space(xCoordi, yCoordi) {
   this.xCoordi = xCoordi;
   this.yCoordi = yCoordi;
   this.selection = null;
 };
 
+// Boards use a custom function to build a board of Spaces.
 function Board() {
   this.spaces = boardBuild();
 }
 
-function Game(selection1, selection2) {
-  this.turns = 0;
-  this.board = new Board();
-  this.player1 = new Player(selection1);
-  this.player2 = new Player(selection2);
-}
-
-
-
+// A function to mark the board!
 Board.prototype.mark = function(xInput, yInput, mark) {
   this.spaces.forEach(function(space) {
     if (space.xCoordi === xInput) {
@@ -34,15 +29,14 @@ Board.prototype.mark = function(xInput, yInput, mark) {
   return false;
 };
 
+// A function to check if there is a win on the board. Don't mess with this, it's perfect.
 Board.prototype.winCheck = function() {
   var combinations = [[0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 5 ,8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [6, 4, 2]]
   var board = this;
   var win = false;
-
   combinations.forEach(function(combo) {
     var mark = [];
     combo.forEach(function(coordiPair) {
-
       mark.push(board.spaces[coordiPair].selection);
     });
     if (mark[0] !== null && mark[1] !== null && mark[2] !== null) {
@@ -54,16 +48,30 @@ Board.prototype.winCheck = function() {
   return win;
 }
 
+
+// A Game starts out with zero turns, creates a blank Board, and applies
+// selections (whatever string you want here) for each player to be created.
+// The Game tracks two players. This could be a good place to track turns?
+function Game(selection1, selection2) {
+  this.turns = 0;
+  this.board = new Board();
+  this.player1 = new Player(selection1);
+  this.player2 = new Player(selection2);
+}
+
+// A function to control for tie games. Where would you use this?
 Game.prototype.tieStatus = function() {
   if (this.board.winCheck() === false && this.turns === 8) {
     return "Tie";
   }
 }
 
+// A function to increase the turn count of the game, currently not tied to any players.
 Game.prototype.turnCount = function() {
   this.turns += 1;
 }
 
+// Where we build the board of nine spaces.
 var boardBuild = function() {
   var board = [];
   for (var y = 0; y < 3; y++) {
